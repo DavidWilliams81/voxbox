438b6ae173144f4037981e33f337e2117b2e2669
David Williams <DavidWilliams81@users.noreply.github.com>
2017-11-04 08:18:51 +0000

    Split test code from writing code.

---
+++ waves.py
@@ -0,0 +1,46 @@
+import numpy as np
+import scipy as sp
+
+import magicavoxel
+
+def waves(freq, height):
+    x,y = np.mgrid[0:row_count, 0:col_count]
+    dist = np.hypot(x * freq, y * freq)
+    #result = np.sin(dist) / np.sqrt(dist)
+    result = sp.special.j0(dist)
+    result += 0.4
+    result *= (height / 1.4)
+    return result
+
+row_count = 126
+col_count = 126
+slice_count = 64
+
+# See https://goo.gl/AP753K
+# See https://goo.gl/W15gma
+# See https://goo.gl/AXRypy
+#x,y = np.mgrid[0:row_count, 0:col_count]
+#height=np.sinc(np.hypot(x / row_count,y / col_count))
+#height *= 39
+
+height = waves(0.4, slice_count)
+
+v = np.zeros((slice_count, col_count, row_count), dtype=np.uint8)
+v[0x00][0x1a][0x0a] = 0x4f
+ 
+for slice in range(0, slice_count):
+    for col in range(0, col_count):
+        for row in range(0, row_count):
+            if slice < height[col][row]:
+                v[slice][col][row] = 0x4f
+    
+    
+result = magicavoxel.write_magicavoxel(v)
+
+
+#data = bytearray(b'TEST')
+#write_main_chunk(data)
+#
+file = open("filename.vox", "wb")
+file.write(result)
+file.close()
