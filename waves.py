62dcc5dee1f1339dc8877b21d8f0159e5c817e4f
David Williams <DavidWilliams81@users.noreply.github.com>
2017-11-05 23:20:39 +0000

    Tidying up example.

--- waves.py
+++ waves.py
@@ -1,71 +1,74 @@
-import numpy as np
-import scipy as sp
-
+# For generating our heightmap
 import math
+import numpy as np
 
-import voxbox.magicavoxel
+# For saving the result
 import voxbox.util
+import voxbox.magicavoxel
 
+"""
+Generates a heightmap image from the Sinc function. This could be implemented
+faster using NumPy functions but it's just an example, and I think the raw
+python is clearer.
+"""
 def generate_waves_heightmap(row_count, col_count):
     
-    freq = 0.4    
+    freq = 0.4 # Controls spacing between waves.
     centre_row = (row_count / 2.0) + 0.5
-    centre_col = (col_count / 2.0) + 0.5
-    
+    centre_col = (col_count / 2.0) + 0.5    
     result = np.zeros([col_count, row_count])
     
+    # For each pixel in the 2D heightmap
     for col in range(0, col_count):
         for row in range(0, row_count):
             
+            # Find the distance between the centre and the current pixel.
             col_dist = col - centre_col
             row_dist = row - centre_row
-            dist = math.sqrt(row_dist * row_dist + col_dist * col_dist)
-            
+            dist = math.sqrt(row_dist * row_dist + col_dist * col_dist)            
             dist *= freq
             
+            # Sinc function
             result[col][row] = math.sin(dist) / dist
             
-            #result[col][row] += 0.3
-            #result[col][row] /= 1.4
+            # Scale to the zero to one range.
+            sinc_minima = 0.2122
+            result[col][row] += sinc_minima
+            result[col][row] /= (1.0 + sinc_minima)
                   
     return result
-    
-#            x,y = np.mgrid[0:row_count, 0:col_count]
-#            dist = np.hypot(x * freq, y * freq)
-#            result = np.sin(dist) / np.sqrt(dist)
-#            #result = sp.special.j0(dist)
-#            result += 0.4
-#            result /= 1.4
-#            return result
 
+# Define the size of the volume
 row_count = 126
 col_count = 126
 plane_count = 64
 
-# See https://goo.gl/AP753K
-# See https://goo.gl/W15gma
-# See https://goo.gl/AXRypy
-#x,y = np.mgrid[0:row_count, 0:col_count]
-#height=np.sinc(np.hypot(x / row_count,y / col_count))
-#height *= 39
-
+# Create a simple heightmap (could also load something from disk)
 heightmap = generate_waves_heightmap(row_count, col_count)
 
+# Create a NumPy array
 voxels = np.zeros((plane_count, col_count, row_count), dtype=np.uint8)
-voxels[0x00][0x1a][0x0a] = 0x4f
- 
+
+# Select a material (white in default palette)
+material_index = 1
+
+# For each voxel in the volume
 for plane in range(0, plane_count):
     for col in range(0, col_count):
         for row in range(0, row_count):
             
+            # Get the height from the heightmap, and
+            # scale to the height of the volume
             height = heightmap[col, row]
             height *= plane_count
             
-            if plane < height:
-                voxels[plane][col][row] = 0x4f
+            # If the current voxel is below the
+            # heightmap then set it to be solid.
+            if plane <= height:
+                voxels[plane][col][row] = material_index 
     
 
-# Save the volume to disk
+# Save the volume to disk as a MagicaVoxel file.
 filename = "waves.vox"
 voxbox.magicavoxel.write(voxels, filename)
 
