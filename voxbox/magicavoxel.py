3e78f3395521e69fe259b499d5451e5b0d5cac1f
David Williams <DavidWilliams81@users.noreply.github.com>
2017-11-06 22:30:54 +0000

    Work on palette writing.

--- voxbox/magicavoxel.py
+++ voxbox/magicavoxel.py
@@ -7,15 +7,22 @@ import voxbox.util
 def write_chunk(id, chunk_content, child_chunks):
     
     data = bytearray(id)
-    data = data + struct.pack('ii', len(chunk_content), len(child_chunks))
-    data = data + chunk_content
-    data = data + child_chunks
+    
+    chunk_content_length = len(chunk_content) if chunk_content else 0
+    child_chunks_length = len(child_chunks) if child_chunks else 0
+    data = data + struct.pack('ii', chunk_content_length, child_chunks_length)
+    
+    if chunk_content:
+        data = data + chunk_content
+        
+    if child_chunks:
+        data = data + child_chunks
     return data
 
 def write_size_chunk(volume):
     
     chunk_content = struct.pack('iii', len(volume[0][0]), len(volume[0]), len(volume))
-    return write_chunk(b'SIZE', chunk_content, bytearray())
+    return write_chunk(b'SIZE', chunk_content, None)
     
 def write_xyzi_chunk(volume):
     
@@ -28,35 +35,35 @@ def write_xyzi_chunk(volume):
     chunk_content = struct.pack('i', len(values))
     chunk_content += bytearray(voxels.tobytes())
                     
-    return write_chunk(b'XYZI', chunk_content, bytearray())
+    return write_chunk(b'XYZI', chunk_content, None)
     
 def write_pack_chunk(volume):
     
     chunk_content = struct.pack('i', 1) # 1 model for now    
-    return write_chunk(b'PACK', chunk_content, bytearray())
-
-
-def write_main_chunk(volume):
-
-    pack_chunk_data = write_pack_chunk(volume)
-    size_chunk_data = write_size_chunk(volume)
-    xyzi_chunk_data = write_xyzi_chunk(volume)
-    children_chunk_size = len(pack_chunk_data) + len(size_chunk_data) + len(xyzi_chunk_data)
-
-    chunk_size = 0;    
-    data = bytearray(b'MAIN')
-    data = data + struct.pack('ii', chunk_size, children_chunk_size)
-    data = data + pack_chunk_data
-    data = data + size_chunk_data
-    data = data + xyzi_chunk_data
+    return write_chunk(b'PACK', chunk_content, None)
     
-    return data
+def write_rgba_chunk(palette):
     
-def write(volume, filename):
+    chunk_content = bytearray(palette[1:256].tobytes())
+    chunk_content += struct.pack('xxxx')    
+    return write_chunk(b'RGBA', chunk_content, None) 
+
+def write_main_chunk(volume, palette):
+    
+    child_chunks  = write_pack_chunk(volume)
+    child_chunks += write_size_chunk(volume)
+    child_chunks += write_xyzi_chunk(volume)
+    
+    if palette != None:
+        child_chunks += write_rgba_chunk(palette)
+    
+    return write_chunk(b'MAIN', None, child_chunks)
+    
+def write(volume, filename, palette = None):
     
     data = bytearray(b'VOX ')
     data = data + struct.pack('i', 150);
-    data = data + write_main_chunk(volume)
+    data = data + write_main_chunk(volume, palette)
     
     file = open(filename, "wb")
     file.write(data)
@@ -70,8 +77,12 @@ if __name__ == "__main__":
     
     voxels = np.zeros((plane_count, col_count, row_count), dtype=np.uint8)
     voxels[0x00][0x1a][0x0a] = 0x4f
+    
+    palette = np.zeros((256, 4), dtype=np.uint8)
+    palette[1] = [255, 0, 0, 255]
+    palette[255] = [255, 255, 255, 255]
           
     filename = "test_magicavoxel_write.vox"
-    write(voxels, filename)
+    write(voxels, filename, palette)
     voxbox.util.open_in_default_app(filename)
     
