123a4174e2879825ca2a64f53cb7825f750299ce
David Williams <DavidWilliams81@users.noreply.github.com>
2018-01-08 10:57:10 +0000

    Moved test to separate folder.

--- voxbox/magicavoxel.py
+++ voxbox/magicavoxel.py
@@ -180,36 +180,4 @@ def read(filename):
     volume_list = read_main_chunk(file)
     
     return volume_list
-
-
-if __name__ == "__main__":
-    
-    filename = "horse.vox"
-    volume_list, palette = read(filename)
-
-    palette[1]   = [255, 0,   0,   255] # Red
-    palette[2]   = [0,   255, 0,   255] # Green
-    palette[3]   = [0,   0,   255, 255] # Blue
-    palette[255] = [255, 255, 255, 255] # White
-    
-    frame_count = len(volume_list)
-    
-    for frame_index, voxels in enumerate(volume_list):
-        
-        (plane_count, row_count, col_count) = voxels.shape
-              
-        for col in range(frame_index, col_count, frame_count):
-            voxels[0][0][col] = 1 # Red
-            
-        for row in range(frame_index, row_count, frame_count):
-            voxels[0][row][0] = 2 # Green
-                  
-        for plane in range(frame_index, plane_count, frame_count):
-            voxels[plane][0][0] = 3 #Blue
-                  
-        voxels[0][0][0] = 255 # White
-          
-    filename = "test_magicavoxel_write.vox"
-    write(volume_list, filename, palette)
-    voxbox.util.open_in_default_app(filename)
-    
+ 
