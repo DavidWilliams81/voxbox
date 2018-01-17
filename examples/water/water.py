fcc23aa1280334e0ebbeeb27dcdabd4cf919669c
David Williams <DavidWilliams81@users.noreply.github.com>
2018-01-17 23:20:53 +0000

    Switched to using draw_box function from geometry module.

--- examples/water/water.py
+++ examples/water/water.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import scipy.ndimage.filters
 
+import voxbox.geometry
+
 # For saving the result
 import voxbox.util
 import voxbox.magicavoxel
@@ -34,23 +36,6 @@ heightmap -= 0.5
 heightmap *= 0.2
 heightmap += 0.5
 
-def checkered_box(volume, lower_corner, upper_corner, col0, col1):
-    
-    for plane in range(lower_corner[0], upper_corner[0] + 1):
-        for col in range(lower_corner[1], upper_corner[1] + 1):
-            for row in range(lower_corner[2], upper_corner[2] + 1):
-                
-                p = (int(plane) // 8) % 2
-                c = (int(col) // 8) % 2
-                r = (int(row) // 8) % 2
-                    
-                val = p ^ c ^ r
-                
-                if val == 0:                
-                    volume[plane][col][row] = col0
-                else:
-                    volume[plane][col][row] = col1
-
 # Start with an empty list of volumes
 volume_list = []
 
@@ -61,15 +46,15 @@ for frame in range(0, frame_count):
     
     volume = np.zeros((plane_count, col_count, row_count), dtype=np.uint8)
     
-    checkered_box(volume, (0, 0, 0), (0, row_count-1, col_count-1), 246, 252)
+    voxbox.geometry.draw_box(volume, (0, 0, 0), (0, row_count-1, col_count-1), 246, 252)
     
-    checkered_box(volume, (0, 0, 0), (plane_count-25, 0, col_count-1), 246, 252)
-    checkered_box(volume, (0, row_count-1, 0), (plane_count-25, row_count-1, col_count-1), 246, 252)
+    voxbox.geometry.draw_box(volume, (0, 0, 0), (plane_count-25, 0, col_count-1), 246, 252)
+    voxbox.geometry.draw_box(volume, (0, row_count-1, 0), (plane_count-25, row_count-1, col_count-1), 246, 252)
     
-    checkered_box(volume, (0, 0, 0), (plane_count-25, row_count - 1, 0), 246, 252)
-    checkered_box(volume, (0, 0, col_count - 1), (plane_count-25, row_count - 1, col_count - 1), 246, 252)
+    voxbox.geometry.draw_box(volume, (0, 0, 0), (plane_count-25, row_count - 1, 0), 246, 252)
+    voxbox.geometry.draw_box(volume, (0, 0, col_count - 1), (plane_count-25, row_count - 1, col_count - 1), 246, 252)
     
-    checkered_box(volume, (0, 50, 50), (plane_count - 1, 76, 76), 246, 217)
+    voxbox.geometry.draw_box(volume, (0, 50, 50), (plane_count - 1, 76, 76), 246, 217)
 
     for plane in range(0, plane_count):
         for col in range(0, col_count):
