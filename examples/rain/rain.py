d644d45ed978249a4c823d4731618396e943eb77
David Williams <DavidWilliams81@users.noreply.github.com>
2018-03-23 23:17:38 +0000

    Work on rain effect.

--- examples/rain/rain.py
+++ examples/rain/rain.py
@@ -24,6 +24,7 @@ blue_noise_data = blue_noise_data.reshape((64,64,64))
 
 random.seed(12345)  
 
+frame_count = 10
 drop_count = 10000
 drop_length = 3
 drop_speed = 3
@@ -31,9 +32,8 @@ drop_speed = 3
 filename = "C:/Users/David/Downloads/MagicaVoxel-0.98.2-win/MagicaVoxel-0.98.2-win/vox/monu10.vox"
 volume_list, palette = voxbox.magicavoxel.read(filename)
 
-model_volume = volume_list[0]
 
-rain_volume = np.zeros(model_volume.shape, dtype=np.uint8)
+rain_volume = np.zeros((frame_count, 72,72), dtype=np.uint8)
 
 for col in range(rain_volume.shape[2]):
     for row in range(0,rain_volume.shape[1]):
@@ -50,25 +50,35 @@ for col in range(rain_volume.shape[2]):
 #    
 #    rain_volume[plane][row][col] = 255
                
-plt.imshow(rain_volume[0], cmap="Greys_r", interpolation="nearest")
-              
-for i in range(drop_length - 1):
-    for col in range(model_volume.shape[2]):
-        for row in range(0,model_volume.shape[1]):
-            for plane in range(0,model_volume.shape[0]):
-                if(rain_volume[(plane+1)%model_volume.shape[0]][row][col]) > 0:
-                    rain_volume[plane][row][col] = 255
+#plt.imshow(rain_volume[0], cmap="Greys_r", interpolation="nearest")
+#              
+#for i in range(drop_length - 1):
+#    for col in range(model_volume.shape[2]):
+#        for row in range(0,model_volume.shape[1]):
+#            for plane in range(0,model_volume.shape[0]):
+#                if(rain_volume[(plane+1)%model_volume.shape[0]][row][col]) > 0:
+#                    rain_volume[plane][row][col] = 255
 
-for col in range(model_volume.shape[2]):
+model_volumes = []
+
+for frame in range(frame_count):
+    
+    model_volume = np.copy(volume_list[0])
+
+    for col in range(model_volume.shape[2]):
         for row in range(model_volume.shape[1]):
             for plane in reversed(range(model_volume.shape[0])):
                 
                 if(model_volume[plane][row][col]) > 0:
                     break
                 
-                model_volume[plane][row][col] = rain_volume[plane][row][col]
+                model_volume[plane][row][col] = rain_volume[plane % frame_count][row][col]
+                
+    model_volumes.append(model_volume)
+    
+    rain_volume = np.roll(rain_volume, -1, axis=0)
 
       
 filename = "rain.vox"
-voxbox.magicavoxel.write([model_volume], filename, palette)
+voxbox.magicavoxel.write(model_volumes, filename, palette)
 voxbox.util.open_in_default_app(filename)
