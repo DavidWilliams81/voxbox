5a7167373924bf547f2387a079f8ffef9ac8382d
David Williams <DavidWilliams81@users.noreply.github.com>
2018-03-25 10:15:42 +0100

    Optimizations and adding splashes.

--- examples/rain/rain.py
+++ examples/rain/rain.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import voxbox.geometry
 import voxbox.util
 import voxbox.magicavoxel
 
@@ -10,6 +11,8 @@ import random
 
 import os
 
+import time
+
 f = open("HDR_L.raw", "rb")
 f.seek(4*6, os.SEEK_SET)
 
@@ -18,7 +21,7 @@ blue_noise_data = blue_noise_data.reshape((64,64,64))
 
 random.seed(12345)  
 
-frame_count = 10
+frame_count = 5
 drop_count = 200
 drop_length = 5
 drop_speed = 3
@@ -66,26 +69,75 @@ for i in range(drop_length - 1):
             for plane in range(0,rain_volume.shape[0]):
                 if(rain_volume[(plane+1)%rain_volume.shape[0]][row][col]) > 0:
                     rain_volume[plane][row][col] = 255
+                               
+tiles = src_volume.shape[0] // rain_volume.shape[0] + 1
+                        
+rain_volume = np.tile(rain_volume,(tiles, 1, 1))
 
 model_volumes = []
 
+t0 = time.time()
+
+rain_mask = np.zeros(src_volume.shape, dtype=np.bool)
+
+for col in range(src_volume.shape[2]):
+    for row in range(src_volume.shape[1]):
+        for plane in reversed(range(src_volume.shape[0])):
+            
+            if(src_volume[plane][row][col]) > 0:
+                break
+            
+            rain_mask[plane][row][col] = True
+
 for frame in range(frame_count):
     
+    print("Generating frame {}".format(frame))
     model_volume = np.copy(src_volume)
 
+    
     for col in range(model_volume.shape[2]):
         for row in range(model_volume.shape[1]):
-            for plane in reversed(range(model_volume.shape[0])):
+            for plane in reversed(range(model_volume.shape[0])): 
                 
-                if(model_volume[plane][row][col]) > 0:
+                if rain_mask[plane][row][col]:  
+                    if rain_volume[plane][row][col] == 255:
+                        model_volume[plane][row][col] = 255
+                else:
+                    
+                    if rain_volume[plane][row][col] == 255:
+                        if plane < 120:
+                            if row > 0 and row < 125 and col > 0 and col < 125:
+                                
+                                model_volume[plane+1][row-1][col-1] = 255
+                                model_volume[plane+1][row-1][col+0] = 255
+                                model_volume[plane+1][row-1][col+1] = 255
+                                            
+                                model_volume[plane+1][row+0][col-1] = 255
+                                #model_volume[plane+1][row+0][col+0] = 255
+                                model_volume[plane+1][row+0][col+1] = 255
+                                            
+                                model_volume[plane+1][row+1][col-1] = 255
+                                model_volume[plane+1][row+1][col+0] = 255
+                                model_volume[plane+1][row+1][col+1] = 255
+                    
                     break
-                
-                model_volume[plane][row][col] = rain_volume[plane % (frame_count*drop_speed)][row][col]
+                    
+#                if rain_mask[plane][row][col] == 1:
+#                    if rain_volume[plane][row][col] == 255:
+#                        if plane > 0:
+#                            if row > 0 and row < 125 and col > 0 and col < 125:
+#                                model_volume[plane-1][row-1][col-1] = 255
+#                                model_volume[plane-1][row-1][col+1] = 255
+#                                model_volume[plane-1][row+1][col-1] = 255
+#                                model_volume[plane-1][row+1][col+1] = 255
                 
     model_volumes.append(model_volume)
     
     rain_volume = np.roll(rain_volume, -drop_speed, axis=0)
 
+t1 = time.time()
+
+print("Done in {}".format(t1-t0))
       
 filename = "rain.vox"
 voxbox.magicavoxel.write(model_volumes, filename, palette)
